2fea43046f084e34827317debad3987a52e1f32b
crcms <crcms@crcms.cn>
2018-06-15 07:25:48 +0800

    array related functions

--- src/App/Helpers/helper.php
+++ src/App/Helpers/helper.php
@@ -36,41 +36,103 @@ function sub_domain(string $sub): string
  * @param array $array2
  * @return array
  */
-function array_merge_recursive_adv(array &$array1, array $array2) {
-    foreach ($array2 as $key => $value) {
-        if (array_key_exists($key, $array1)) {
-            if (is_array($value)) {
-                array_merge_recursive_adv($array1[$key], $value);
-            } else {
-                if (!empty($array1[$key])) {
-                    if (is_array($array1[$key])) {
-                        array_push($array1[$key], $value);
-                    } else {
-                        $array1[$key] = [$array1[$key]];
-                        $array1[$key][] = $value;
-                    }
-                } else if (empty($array1[$key])) {
-                    $array1[$key] = $value;
-                }
+//function array_merge_recursive_adv(array &$array1, array $array2) {
+//    foreach ($array2 as $key => $value) {
+//        if (array_key_exists($key, $array1)) {
+//            if (is_array($value)) {
+//                array_merge_recursive_adv($array1[$key], $value);
+//            } else {
+//                if (!empty($array1[$key])) {
+//                    if (is_array($array1[$key])) {
+//                        array_push($array1[$key], $value);
+//                    } else {
+//                        $array1[$key] = [$array1[$key]];
+//                        $array1[$key][] = $value;
+//                    }
+//                } else if (empty($array1[$key])) {
+//                    $array1[$key] = $value;
+//                }
+//            }
+//        } else {
+//            $array1[$key] = $value;
+//        }
+//    }
+//    return $array1;
+//}
+
+/**
+ * @param array
+ * @param array
+ * @return array
+ */
+function array_merge_recursive_distinct(): array
+{
+    $arrays = func_get_args();
+    $base = array_shift($arrays);
+    if (!is_array($base)) $base = empty($base) ? array() : array($base);
+    foreach ($arrays as $append) {
+        if (!is_array($append)) $append = array($append);
+        foreach ($append as $key => $value) {
+            if (!array_key_exists($key, $base) and !is_numeric($key)) {
+                $base[$key] = $append[$key];
+                continue;
             }
-        } else {
-            $array1[$key] = $value;
+            if (is_array($value) or is_array($base[$key])) {
+                $base[$key] = array_merge_recursive_distinct($base[$key], $append[$key]);
+            } else if (is_numeric($key)) {
+                if (!in_array($value, $base)) $base[] = $value;
+            } else {
+                $base[$key] = $value;
+            }
         }
     }
-    return $array1;
+    return $base;
+}
+
+/**
+ * @param array
+ * @param array
+ * @return array
+ */
+function array_merge_recursive_adv(): array
+{
+    if (func_num_args() < 2) {
+        trigger_error(__FUNCTION__ . ' needs two or more array arguments', E_USER_WARNING);
+        return [];
+    }
+    $arrays = func_get_args();
+    $merged = array();
+    while ($arrays) {
+        $array = array_shift($arrays);
+        if (!is_array($array)) {
+            trigger_error(__FUNCTION__ . ' encountered a non array argument', E_USER_WARNING);
+            return [];
+        }
+        if (!$array)
+            continue;
+        foreach ($array as $key => $value)
+            if (is_string($key))
+                if (is_array($value) && array_key_exists($key, $merged) && is_array($merged[$key]))
+                    $merged[$key] = call_user_func(__FUNCTION__, $merged[$key], $value);
+                else
+                    $merged[$key] = $value;
+            else
+                $merged[] = $value;
+    }
+    return $merged;
 }
 
 function framework_path($path = '')
 {
-    return app('path.framework').($path ? DIRECTORY_SEPARATOR.$path : $path);
+    return app('path.framework') . ($path ? DIRECTORY_SEPARATOR . $path : $path);
 }
 
 function framework_config_path($path = '')
 {
-    return app('path.framework_config').($path ? DIRECTORY_SEPARATOR.$path : $path);
+    return app('path.framework_config') . ($path ? DIRECTORY_SEPARATOR . $path : $path);
 }
 
 function framework_resource_path($path = '')
 {
-    return app('path.framework_resource').($path ? DIRECTORY_SEPARATOR.$path : $path);
+    return app('path.framework_resource') . ($path ? DIRECTORY_SEPARATOR . $path : $path);
 }
