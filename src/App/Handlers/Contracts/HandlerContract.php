fc0a5b006493aed4eb80284b7a87a07d4276208f
simon <434730525@qq.com>
2018-09-08 10:29:45 +0800

    Handler interface

--- src/App/Handlers/Contracts/HandlerContract.php
+++ src/App/Handlers/Contracts/HandlerContract.php
@@ -2,10 +2,13 @@
 
 namespace CrCms\Foundation\App\Handlers\Contracts;
 
+use CrCms\Foundation\Transporters\Contracts\DataProviderContract;
+
 interface HandlerContract
 {
     /**
+     * @param DataProviderContract $provider
      * @return mixed
      */
-//    public function handle();
+    public function handle(DataProviderContract $provider);
 }
