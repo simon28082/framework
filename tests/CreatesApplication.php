b0fa7df0e7f3bdf8a59e7e9520877316d62ba20f
crcms <crcms@crcms.cn>
2018-10-22 17:19:54 +0800

    testing application

---
+++ tests/CreatesApplication.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace CrCms\Foundation\Tests;
+
+use CrCms\Foundation\Start;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Contracts\Console\Kernel;
+
+trait CreatesApplication
+{
+    /**
+     * @var string
+     */
+    protected $mode = 'laravel';
+
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $start = Start::instance();
+
+        $start->bootstrap($this->mode);
+
+        $start->getApp()->make(Kernel::class)->bootstrap();
+
+        Hash::driver('bcrypt')->setRounds(4);
+
+        return $start->getApp();
+    }
+}
