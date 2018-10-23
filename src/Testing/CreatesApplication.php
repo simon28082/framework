02201062195c63fb77fc7096843a5a5e427f8799
crcms <crcms@crcms.cn>
2018-10-23 08:59:47 +0800

    testing

---
+++ src/Testing/CreatesApplication.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace CrCms\Foundation\Testing;
+
+use CrCms\Foundation\Start;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Contracts\Console\Kernel;
+
+trait CreatesApplication
+{
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $start = Start::instance();
+
+        $start->bootstrap($this->mode());
+
+        $start->getApp()->make(Kernel::class)->bootstrap();
+
+        Hash::driver('bcrypt')->setRounds(4);
+
+        return $start->getApp();
+    }
+
+    /**
+     * @return string
+     */
+    protected function mode(): string
+    {
+        return isset($this->mode) && !is_null($this->mode) ? $this->mode : 'laravel';
+    }
+}
