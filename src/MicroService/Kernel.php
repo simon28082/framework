8bb04ce66deaca711257f0f88fb79558a83c29f5
crcms <crcms@crcms.cn>
2018-11-09 21:36:53 +0800

    enable exception

--- src/MicroService/Kernel.php
+++ src/MicroService/Kernel.php
@@ -43,7 +43,7 @@ class Kernel implements KernelContract
     protected $bootstrappers = [
         \Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables::class,
         \Illuminate\Foundation\Bootstrap\LoadConfiguration::class,
-//        \Illuminate\Foundation\Bootstrap\HandleExceptions::class,
+        \Illuminate\Foundation\Bootstrap\HandleExceptions::class,
         \Illuminate\Foundation\Bootstrap\RegisterFacades::class,
         \Illuminate\Foundation\Bootstrap\RegisterProviders::class,
         \Illuminate\Foundation\Bootstrap\BootProviders::class,
